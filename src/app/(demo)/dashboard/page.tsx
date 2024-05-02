7bfecfebdbb21c643059f2f65fa85dfbbdcb5ee6
Mohamad Salimi <contact@salimi.my>
2024-05-03 04:10:16 +0800

    Update dashboard page

--- src/app/(demo)/dashboard/page.tsx
+++ src/app/(demo)/dashboard/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
+import PlaceholderContent from "@/components/demo/placeholder-content";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent />
+    </>
+  );
 }
