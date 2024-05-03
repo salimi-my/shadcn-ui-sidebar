d02e7acaccee1d85c69f57e5425a861166b2a336
Mohamad Salimi <contact@salimi.my>
2024-05-03 18:58:29 +0800

    Add scrollarea to sidebar

--- src/components/demo/menu.tsx
+++ src/components/demo/menu.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { getPages } from "@/lib/pages";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { CollapseMenuButton } from "@/components/demo/collapse-menu-button";
 import {
   Tooltip,
@@ -24,109 +25,114 @@ export function Menu({ isOpen }: MenuProps) {
   const pages = getPages(pathname);
 
   return (
-    <nav className="mt-8 h-full">
-      <ul className="flex flex-col h-full items-start space-y-1 px-2">
-        {pages.map(({ groupLabel, menus }, index) => (
-          <li className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
-            {(isOpen && groupLabel) || isOpen === undefined ? (
-              <p className="text-sm font-medium text-muted-foreground px-4 pb-2">
-                {groupLabel}
-              </p>
-            ) : !isOpen && isOpen !== undefined && groupLabel ? (
-              <TooltipProvider>
-                <Tooltip delayDuration={100}>
-                  <TooltipTrigger className="w-full">
-                    <div className="w-full flex justify-center items-center">
-                      <Ellipsis className="h-5 w-5" />
-                    </div>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>{groupLabel}</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ) : (
-              <p className="pb-2"></p>
-            )}
-            {menus.map(({ href, label, icon: Icon, active, submenus }, index) =>
-              submenus.length === 0 ? (
-                <div className="w-full" key={index}>
-                  <TooltipProvider disableHoverableContent>
-                    <Tooltip delayDuration={100}>
-                      <TooltipTrigger asChild>
-                        <Button
-                          variant={active ? "secondary" : "ghost"}
-                          className="w-full justify-start h-10"
-                          asChild
-                        >
-                          <Link href={href}>
-                            <span
-                              className={cn(isOpen === false ? "" : "mr-4")}
-                            >
-                              <Icon size={18} />
-                            </span>
-                            <p
-                              className={cn(
-                                "whitespace-nowrap",
-                                isOpen === false
-                                  ? "-translate-x-96 opacity-0"
-                                  : "translate-x-0 opacity-100"
-                              )}
-                            >
-                              {label}
-                            </p>
-                          </Link>
-                        </Button>
-                      </TooltipTrigger>
-                      {isOpen === false && (
-                        <TooltipContent side="right">{label}</TooltipContent>
-                      )}
-                    </Tooltip>
-                  </TooltipProvider>
-                </div>
+    <ScrollArea className="[&>div>div[style]]:!block">
+      <nav className="mt-8 h-full w-full">
+        <ul className="flex flex-col min-h-[calc(100vh-48px-36px-16px-32px)] lg:min-h-[calc(100vh-32px-40px-32px)] items-start space-y-1 px-2">
+          {pages.map(({ groupLabel, menus }, index) => (
+            <li className={cn("w-full", groupLabel ? "pt-5" : "")} key={index}>
+              {(isOpen && groupLabel) || isOpen === undefined ? (
+                <p className="text-sm font-medium text-muted-foreground px-4 pb-2">
+                  {groupLabel}
+                </p>
+              ) : !isOpen && isOpen !== undefined && groupLabel ? (
+                <TooltipProvider>
+                  <Tooltip delayDuration={100}>
+                    <TooltipTrigger className="w-full">
+                      <div className="w-full flex justify-center items-center">
+                        <Ellipsis className="h-5 w-5" />
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{groupLabel}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               ) : (
-                <div className="w-full" key={index}>
-                  <CollapseMenuButton
-                    icon={Icon}
-                    label={label}
-                    active={active}
-                    submenus={submenus}
-                    isOpen={isOpen}
-                  />
-                </div>
-              )
-            )}
-          </li>
-        ))}
-        <li className="w-full grow flex items-end">
-          <TooltipProvider disableHoverableContent>
-            <Tooltip delayDuration={100}>
-              <TooltipTrigger asChild>
-                <Button
-                  onClick={() => {}}
-                  variant="outline"
-                  className="w-full justify-center h-10 mt-4"
-                >
-                  <span className={cn(isOpen === false ? "" : "mr-4")}>
-                    <LogOut size={18} />
-                  </span>
-                  <p
-                    className={cn(
-                      "whitespace-nowrap",
-                      isOpen === false ? "opacity-0 hidden" : "opacity-100"
-                    )}
-                  >
-                    Sign out
-                  </p>
-                </Button>
-              </TooltipTrigger>
-              {isOpen === false && (
-                <TooltipContent side="right">Sign out</TooltipContent>
+                <p className="pb-2"></p>
               )}
-            </Tooltip>
-          </TooltipProvider>
-        </li>
-      </ul>
-    </nav>
+              {menus.map(
+                ({ href, label, icon: Icon, active, submenus }, index) =>
+                  submenus.length === 0 ? (
+                    <div className="w-full" key={index}>
+                      <TooltipProvider disableHoverableContent>
+                        <Tooltip delayDuration={100}>
+                          <TooltipTrigger asChild>
+                            <Button
+                              variant={active ? "secondary" : "ghost"}
+                              className="w-full justify-start h-10"
+                              asChild
+                            >
+                              <Link href={href}>
+                                <span
+                                  className={cn(isOpen === false ? "" : "mr-4")}
+                                >
+                                  <Icon size={18} />
+                                </span>
+                                <p
+                                  className={cn(
+                                    "whitespace-nowrap",
+                                    isOpen === false
+                                      ? "-translate-x-96 opacity-0"
+                                      : "translate-x-0 opacity-100"
+                                  )}
+                                >
+                                  {label}
+                                </p>
+                              </Link>
+                            </Button>
+                          </TooltipTrigger>
+                          {isOpen === false && (
+                            <TooltipContent side="right">
+                              {label}
+                            </TooltipContent>
+                          )}
+                        </Tooltip>
+                      </TooltipProvider>
+                    </div>
+                  ) : (
+                    <div className="w-full" key={index}>
+                      <CollapseMenuButton
+                        icon={Icon}
+                        label={label}
+                        active={active}
+                        submenus={submenus}
+                        isOpen={isOpen}
+                      />
+                    </div>
+                  )
+              )}
+            </li>
+          ))}
+          <li className="w-full grow flex items-end">
+            <TooltipProvider disableHoverableContent>
+              <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                  <Button
+                    onClick={() => {}}
+                    variant="outline"
+                    className="w-full justify-center h-10 mt-4"
+                  >
+                    <span className={cn(isOpen === false ? "" : "mr-4")}>
+                      <LogOut size={18} />
+                    </span>
+                    <p
+                      className={cn(
+                        "whitespace-nowrap",
+                        isOpen === false ? "opacity-0 hidden" : "opacity-100"
+                      )}
+                    >
+                      Sign out
+                    </p>
+                  </Button>
+                </TooltipTrigger>
+                {isOpen === false && (
+                  <TooltipContent side="right">Sign out</TooltipContent>
+                )}
+              </Tooltip>
+            </TooltipProvider>
+          </li>
+        </ul>
+      </nav>
+    </ScrollArea>
   );
 }
