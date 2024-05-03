c2385d95a12fd276e41a3fba9bfee58c18e55471
Mohamad Salimi <contact@salimi.my>
2024-05-03 20:55:40 +0800

    Update footer border color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function HomePage() {
           </div>
         </div>
       </main>
-      <footer className="py-6 md:py-0 border-t">
+      <footer className="py-6 md:py-0 border-t border-border/40">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
             Built on top of{" "}
