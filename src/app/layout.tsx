b164f631f33d11c1f8148b4924f3b5d4e7083f00
Mohamad Salimi <contact@salimi.my>
2024-05-04 00:24:26 +0800

    Update metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,9 @@ import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.VERCEL_URL
+    process.env.APP_URL
+      ? `${process.env.APP_URL}`
+      : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
       : `http://localhost:${process.env.PORT || 3000}`
   ),
