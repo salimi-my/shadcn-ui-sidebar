b49df8407c2b20520dda9f62a6690857ce804531
Mohamad Salimi <contact@salimi.my>
2024-05-04 00:06:12 +0800

    Update footer to be center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,8 +97,8 @@ export default function HomePage() {
         </div>
       </main>
       <footer className="py-6 md:py-0 border-t border-border/40">
-        <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+        <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
+          <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
             Built on top of{" "}
             <Link
               href="https://ui.shadcn.com"
