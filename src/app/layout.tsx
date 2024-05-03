b2645ce4a4b968b4ca2df32069657fcb0b938446
Mohamad Salimi <contact@salimi.my>
2024-05-04 00:17:20 +0800

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,35 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+
 import "./globals.css";
+
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : `http://localhost:${process.env.PORT || 3000}`
+  ),
   title: "shadcn/ui sidebar",
   description:
-    "A stunning and functional retractable sidebar for Next.js using shadcn/ui complete with desktop and mobile responsiveness."
+    "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
+  alternates: {
+    canonical: "/"
+  },
+  openGraph: {
+    url: "/",
+    title: "shadcn/ui sidebar",
+    description:
+      "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness.",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "shadcn/ui sidebar",
+    description:
+      "A stunning and functional retractable sidebar for Next.js built on top of shadcn/ui complete with desktop and mobile responsiveness."
+  }
 };
 
 export default function RootLayout({
