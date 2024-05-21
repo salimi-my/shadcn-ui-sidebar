4d393d3604581c8eb14de46f6187c2b0042eff6b
Abe Isleem <a.f_isleem@hotmail.com>
2024-05-21 22:03:43 +0300

    Update footer.tsx: fixes backdrop-filter selector

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur">
+    <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
           Built on top of{" "}
