c49b83f703494be8f22768a87ac7a43ddcf5669e
Abe Isleem <a.f_isleem@hotmail.com>
2024-05-21 22:05:38 +0300

    Update navbar.tsx: fixes backdrop-filter selector

--- src/components/admin-panel/navbar.tsx
+++ src/components/admin-panel/navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 export function Navbar({ title }: NavbarProps) {
   return (
-    <header className="z-10 supports-backdrop-blur:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur">
+    <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0">
           <SheetMenu />
