db9cb796e7637f434fa64b3fd0ec91f193841da4
Luis Wehrberger <luis.wehrberger2@gmail.com>
2024-08-18 13:27:29 +0200

    replace h1 with SheetTitle

--- src/components/admin-panel/sheet-menu.tsx
+++ src/components/admin-panel/sheet-menu.tsx
@@ -8,6 +8,7 @@ import {
   SheetHeader,
   SheetContent,
   SheetTrigger,
+  SheetTitle
 } from "@/components/ui/sheet";
 
 export function SheetMenu() {
@@ -27,7 +28,7 @@ export function SheetMenu() {
           >
             <Link href="/dashboard" className="flex items-center gap-2">
               <PanelsTopLeft className="w-6 h-6 mr-1" />
-              <h1 className="font-bold text-lg">Brand</h1>
+              <SheetTitle className="font-bold text-lg">Brand</SheetTitle>
             </Link>
           </Button>
         </SheetHeader>
