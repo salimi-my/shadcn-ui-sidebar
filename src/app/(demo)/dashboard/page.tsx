8402324723700ac7b4110015f2aa4cf92c5cec17
Rot4tion <nhonhoem0123@gmail.com>
2024-10-09 15:42:33 +0700

    add settings to dashboard

--- src/app/(demo)/dashboard/page.tsx
+++ src/app/(demo)/dashboard/page.tsx
@@ -1,6 +1,5 @@
+"use client";
 import Link from "next/link";
-
-import PlaceholderContent from "@/components/demo/placeholder-content";
 import { ContentLayout } from "@/components/admin-panel/content-layout";
 import {
   Breadcrumb,
@@ -10,8 +9,21 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+import { useSidebar } from "@/hooks/use-sidebar";
 
 export default function DashboardPage() {
+  const { settings, setSettings } = useSidebar(({ settings, setSettings }) => ({
+    settings,
+    setSettings
+  }));
   return (
     <ContentLayout title="Dashboard">
       <Breadcrumb>
@@ -27,7 +39,25 @@ export default function DashboardPage() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <PlaceholderContent />
+      <TooltipProvider>
+        <div className="flex gap-6 mt-6">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div className="flex items-center space-x-2">
+                <Switch
+                  id="is-hover-open"
+                  onCheckedChange={(x) => setSettings({ isHoverOpen: x })}
+                  checked={settings.isHoverOpen}
+                />
+                <Label htmlFor="is-hover-open">Hover Open</Label>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>When hovering on the sidebar in mini state, it will open</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      </TooltipProvider>
     </ContentLayout>
   );
 }
